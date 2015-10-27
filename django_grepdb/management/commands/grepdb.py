c9153f4f48cc5fd6eab2e8b0b2b067f4c413fae0
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2015-10-27 23:53:28 +0000

    Move show_values logic into a method

--- django_grepdb/management/commands/grepdb.py
+++ django_grepdb/management/commands/grepdb.py
@@ -12,14 +12,16 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('pattern', type=str, help='Pattern to search for')
         parser.add_argument('identifier', nargs='+', type=str, help='Identifier of an app, model or field')
-        parser.add_argument('--show', '-s', nargs='?', type=int, const=0,
-                            help='Amount of text to show around result; overrides default of not showing results')
+        parser.add_argument('--show-values', '-s', nargs='?', type=int, const=25,
+                            help='Show field value in addition to object representation and pk. ' +
+                                 'Takes optional value of number of chars to show each side of match (default is 25)')
         parser.add_argument('--ignore-case', '-i', action='store_true', help='Match case-insensitively')
 
     def handle(self, **options):
         colorama.init()
         self.pattern = options['pattern']
         self.ignore_case = options['ignore_case']
+        self.show_values = options.get('show_values', False)
         identifiers = options['identifier']
         queries = self.get_queries(identifiers)
         for query in queries:
@@ -28,19 +30,8 @@ class Command(BaseCommand):
                 self.stdout.write(colored(u'\n{}'.format(query['manager'].model), 'cyan', attrs=['bold']))
                 for result in results:
                     self.stdout.write(colored(u'{result} (pk={result.pk})'.format(result=result), 'green', attrs=['bold']))
-                    show = options.get('show', False)
-                    if show is not None:  # can't be a truthiness check, as zero is different from no show
-                        text = getattr(result, query['field_name'])
-                        surrounded_pattern = r'(.{{0,{show}}})({pattern})(.{{0,{show}}})'.format(
-                            show=show, pattern=self.pattern
-                        )
-                        regex_args = [surrounded_pattern, text]
-                        if self.ignore_case:
-                            regex_args.append(re.IGNORECASE)
-                        matches = re.findall(*regex_args)
-                        for pre, match, post in matches:
-                            self.stdout.write(pre + colored(match, 'grey', 'on_yellow') + post)
-                        self.stdout.write('')
+                    if self.show_values is not None:  # can't be a truthiness check, as zero is different from no show
+                        self.stdout.write(self.get_value(result, query))
 
     def get_queries(self, identifiers):
         return [self.get_query(identifier) for identifier in identifiers]
@@ -62,3 +53,15 @@ class Command(BaseCommand):
         if self.ignore_case:
             lookup_type = 'i' + lookup_type
         return {'{field_name}__{lookup_type}'.format(field_name=field_name, lookup_type=lookup_type): self.pattern}
+
+    def get_value(self, result, query):
+        text = getattr(result, query['field_name'])
+        surrounded_pattern = r'(.{{0,{outer}}})({pattern})(.{{0,{outer}}})'.format(
+            outer=self.show_values, pattern=self.pattern
+        )
+        regex_args = [surrounded_pattern, text]
+        if self.ignore_case:
+            regex_args.append(re.IGNORECASE)
+        matches = re.findall(*regex_args)
+        for pre, match, post in matches:
+             return pre + colored(match, 'grey', 'on_yellow') + post + '\n\n'
