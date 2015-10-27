9630b79408b9d534fa817b2f037e0fcffa8c6bd2
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2015-10-27 18:46:02 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-grep-db',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     install_requires=['django>=1.7', 'colorama>=0.3.3', 'termcolor>=1.1.0'],
 
